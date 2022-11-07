91bc6af36f018d7915a26309061dcd0cc82bea04
Tris <gingertris1@gmail.com>
2022-11-07 03:25:03 +0000

    still implementing services

---
+++ packages/services/InviteService.ts
@@ -0,0 +1,39 @@
+import prisma from "../db/PrismaClient";
+
+export const createInvite = async (playerId:string, teamId: number) => {
+    return await prisma.invite.create({
+        data:{
+            teamId:teamId,
+            playerId:playerId
+        }
+    })
+}
+
+export const fetchInvite = async (playerId: string, teamId: number) => {
+    return await prisma.invite.findUnique({
+        where:{
+            teamId_playerId:{
+                teamId:teamId,
+                playerId:playerId
+            }
+        },
+        include:{
+            team:true,
+            player:true
+        }
+    })
+}
+
+export const updateInvite = async (playerId: string, teamId: number, answered: boolean) => {
+    return await prisma.invite.update({
+        where:{
+            teamId_playerId:{
+                teamId:teamId,
+                playerId:playerId
+            }
+        },
+        data:{
+            answered:answered
+        }
+    })
+}
---
+++ packages/services/MatchService.ts
@@ -0,0 +1,23 @@
+import prisma from "../db/PrismaClient";
+
+export const createMatch = async (team1Id: number, team2Id: number, powerHour:boolean) => {
+    return await prisma.match.create({
+        data:{
+            team1Id:team1Id,
+            team2Id:team2Id,
+            powerHour:powerHour
+        }
+    });
+}
+
+export const fetchMatch = async (matchId: number) => {
+    return await prisma.match.findUnique({
+        where:{
+            id:matchId
+        },
+        include:{
+            team1:true,
+            team2:true
+        }
+    })
+}
