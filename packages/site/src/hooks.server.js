7c2899de2af10e5ac761c0664c4015c1ae12aa2e
Tris <gingertris1@gmail.com>
2023-02-07 02:01:54 +0000

    login + navbar start

---
+++ packages/site/src/hooks.server.js
@@ -0,0 +1,9 @@
+import {SvelteKitAuth} from "@auth/sveltekit"
+import Discord from '@auth/core/providers/discord';
+import { DISCORD_ID, DISCORD_SECRET } from "$env/static/private"
+
+export const handle = SvelteKitAuth({
+  providers: [
+    Discord({ clientId: DISCORD_ID, clientSecret: DISCORD_SECRET }),
+  ]
+});
---
+++ packages/site/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { signIn, signOut } from '@auth/sveltekit/client';
+    import { page } from '$app/stores';
+</script>
+
+
+<nav class="navbar">
+	<div class="navbar-brand">
+		<a class="navbar-item" href="/">
+      		<img src="/logo.png" />
+    	</a>
+	</div>
+	
+	<div class="navbar-end">
+		{#if $page.data.session}
+			<span class="navbar-item">
+				Signed in as&#160
+				<strong>{$page.data.session.user.name}</strong>
+			</span>
+			<a class="button is-light navbar-item" on:click={() => signOut()} href='#'>Sign out</a>
+		{:else}
+			<a class="button is-light navbar-item" on:click={()=>signIn('discord')} href='#'>
+				Log in with Discord
+			</a>
+		{/if}
+	</div>
+</nav>
+
